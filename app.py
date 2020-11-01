cea12ae1355b921705634b8643aeaafaed8dfd5f
Hunter Beck <hunter.beck@cognite.com>
2020-11-01 13:45:39 +0100

    added functioning modal form to Explore app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash_bootstrap_components as dbc
 
 app = dash.Dash(
     __name__, 
-    external_stylesheets=[dbc.themes.DARKLY],
+    external_stylesheets=[dbc.themes.LITERA],
     meta_tags=[{"name": "viewport", "content": "width=device-width"}],
     suppress_callback_exceptions=True
 )
--- apps/explore_app.py
+++ apps/explore_app.py
@@ -3,6 +3,7 @@ import dash_html_components as html
 import dash_core_components as dcc
 import dash_table
 from dash.dependencies import State, Input, Output
+from dash.exceptions import PreventUpdate
 from dash import callback_context
 import yaml
 from finance.client import Client  
@@ -13,6 +14,18 @@ with open('config.yml') as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
 client = Client(db_path=config['database']['file_path']) 
+
+accounts = client.accounts.list()
+account_options = [{'label':account.name, 'value':account.id} for account in accounts]
+
+country_codes = client._country_codes
+country_code_options = [{'label':code, 'value': code} for code in country_codes]
+
+labels = client.labels.list()
+labels_options = [{'label':label.name, 'value':label.id} for label in labels]
+
+currency_codes = client._currency_codes
+currency_options = [{'label': code, 'value':code} for code in currency_codes]
 
 ### DATA TYPE SELECTOR ###
 data_type_buttons = dbc.ButtonGroup(
@@ -25,8 +38,6 @@ data_type_buttons = dbc.ButtonGroup(
 )
 
 ### ACCOUNT SELECTION ###
-accounts = client.accounts.list()
-account_options = [{'label':account.name, 'value':account.id} for account in accounts]
 account_dropdown = dcc.Dropdown(
     options=account_options, 
     multi=True, 
@@ -34,9 +45,6 @@ account_dropdown = dcc.Dropdown(
     placeholder='Account'
 )       
 
-country_codes = [account.country_code for account in accounts]
-country_codes = list(set(country_codes))
-country_code_options = [{'label':code, 'value': code} for code in country_codes]
 country_code_dropdown = dcc.Dropdown(
     options=country_code_options, 
     multi=True, 
@@ -44,8 +52,6 @@ country_code_dropdown = dcc.Dropdown(
     placeholder='Country'
 )
     
-labels = client.labels.list()
-labels_options = [{'label':label.name, 'value':label.id} for label in labels]
 label_dropdown = dcc.Dropdown(
     options=labels_options, 
     multi=True, 
@@ -92,11 +98,6 @@ table_placeholder = html.Div(id='table-content', className='mt-3')
 
 TABLE_STYLE = {
     'style_as_list_view':True,
-    'style_header':{'backgroundColor': 'rgb(30, 30, 30)'},
-    'style_cell':{
-        'backgroundColor': 'rgb(50, 50, 50)',
-        'color': 'white'
-    },
     'page_size':20
 }
 
@@ -121,23 +122,139 @@ labels_table = dash_table.DataTable(
     **TABLE_STYLE,
 )
 
+### CREATE POPUP ###
+create_button = dbc.Button("Create", id='create-button', color='info')
+
+create_name_input = dbc.FormGroup(
+    [
+        dbc.Label("Name"),
+        dbc.Input(id='create-name-input', placeholder='Name')
+    ],
+)
+
+create_description_input = dbc.FormGroup(
+    [
+        dbc.Label("Description"),
+        dbc.Input(id='create-description-input', placeholder='Description')
+    ]
+)
+
+create_balance_input = dbc.FormGroup(
+    [
+        dbc.Label("Balance"),
+        dbc.Input(id='create-balance-input', placeholder='Balance')
+    ]
+)
+
+create_account_dropdown = dbc.FormGroup(
+    [
+        dbc.Label("Account"),
+        dcc.Dropdown(
+            id='create-account-dropdown',
+            placeholder='Account',
+            options=account_options
+        )
+    ]
+)
+
+create_label_dropdown = dbc.FormGroup(
+    [
+        dbc.Label("Label"),
+        dcc.Dropdown(
+            id='create-label-dropdown',
+            placeholder='Label',
+            options=labels_options
+        )
+    ]
+)
+
+create_currency_dropdown = dbc.FormGroup(
+    [
+        dbc.Label("Currency"),
+        dcc.Dropdown(
+            id='create-currency-dropdown',
+            placeholder='Currency',
+            options=currency_options
+        )
+    ]
+)
+
+create_country_dropdown = dbc.FormGroup(
+    [
+        dbc.Label("Country"),
+        dcc.Dropdown(
+            id='create-country-dropdown',
+            placeholder='Country',
+            options=country_code_options
+        )
+    ]
+)
+
+account_create_form = dbc.Form([
+    create_name_input,
+    create_description_input,
+    create_label_dropdown,
+    create_country_dropdown
+])
+
+label_create_form = dbc.Form([
+    create_name_input,
+    create_description_input
+])
+
+record_create_form = dbc.Form([
+    create_account_dropdown,
+    create_balance_input,
+    create_currency_dropdown
+])
+
+create_modal = dbc.Modal(
+    [
+        dbc.ModalBody(html.Div(id='create-modal-content')),
+        dbc.ModalFooter(
+            dbc.Button(
+                "Close", id="close-create-modal", className="ml-auto"
+            )
+        ),
+    ],
+    id="create-modal",
+    centered=True,
+)
+
+
 ### BODY ###
 body = html.Div(
     dbc.Row(
         [
             dbc.Col(side_bar, width=3),
-            dbc.Col(table_placeholder, className='mr-4')
+            dbc.Col(
+                [
+                    dbc.Row(
+                        create_button,
+                        justify='end',
+                        className='mt-2'
+                    ),
+                    dbc.Row(
+                        dbc.Col(table_placeholder),
+                        className='mt-2')
+                ], 
+                className='mr-4'
+            )
         ]
     )
 )
 
+### DATA TYPE STORE ###
+data_type_store = dcc.Store(id='data-type-store')
+
 ### LAYOUT ###
-explore_app_layout = html.Div([body])
+explore_app_layout = html.Div([data_type_store, create_modal, body])
 
 ### CALLBACKS ###
 @app.callback(
     [Output('table-content', 'children'),
-     Output('sidebar-content', 'children')],
+     Output('sidebar-content', 'children'),
+     Output('data-type-store', 'data')],
     [Input('accounts-button', 'n_clicks'),
      Input('records-button', 'n_clicks'),
      Input('labels-button', 'n_clicks')]
@@ -147,9 +264,29 @@ def accounts_selection(account_n, record_n, label_n):
     trigger_button = ctx.triggered[0]['prop_id'].split('.')[0]
     
     if trigger_button == 'accounts-button':
-        return accounts_table, account_filter_form
+        return accounts_table, account_filter_form, 'accounts'
     elif trigger_button == 'records-button':
-        return records_table, record_filter_form
+        return records_table, record_filter_form, 'records'
     elif trigger_button == 'labels-button':
-        return labels_table, label_filter_form
+        return labels_table, label_filter_form, 'labels'
+    else:
+        raise PreventUpdate
 
+@app.callback(
+    [Output("create-modal", "is_open"),
+     Output('create-modal-content', 'children')],
+    [Input('create-button', 'n_clicks'),
+     Input("close-create-modal", "n_clicks")],
+    [State("create-modal", "is_open"),
+     State('data-type-store', 'data')],
+)
+def close_modal(create_n, close_n, is_open, data_type_store):
+    
+    if data_type_store == 'accounts':
+        return not is_open, account_create_form
+    elif data_type_store == 'labels':
+        return not is_open, label_create_form
+    elif data_type_store == 'records':
+        return not is_open, record_create_form
+    else:
+        raise PreventUpdate
