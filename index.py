1f44e6f0a4eeadf6d544d0ab45a1b51b7361ba62
Hunter Beck <hunter.beck@cognite.com>
2020-12-13 11:16:21 -0600

    Updated index with configurable port & host and added github action file

--- index.py
+++ index.py
@@ -61,4 +61,4 @@ def display_page(pathname):
         
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True, port=config['app']['port'], host=config['app']['host'])
